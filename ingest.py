4437d5fbcc08ba863a77fa2fcb84bb029cdbfa1e
Krishna Saxena <krishna.saxena@Polestarllp.local>
2026-02-05 01:48:39 +0530

    Initial Commit

---
+++ ingest.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+# Load env variables
+load_dotenv()
+
+# Debug check
+print("Groq key loaded:", bool(os.getenv("GROQ_API_KEY")))
+
+# Groq OpenAI-compatible client
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+# Paths
+AUDIO_PATH = Path("../data/audio/video.mp3")
+TEXT_PATH = Path("../data/text/video.txt")
+
+TEXT_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+print("Starting transcription with Groq...")
+
+with open(AUDIO_PATH, "rb") as audio_file:
+    transcript = client.audio.transcriptions.create(
+        file=audio_file,
+        model="whisper-large-v3"
+    )
+
+with open(TEXT_PATH, "w", encoding="utf-8") as f:
+    f.write(transcript.text)
+
+print("✅ Transcription saved to data/text/video.txt")
---
+++ splitter.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+#from langchain_community.retrievers import MultiQueryRetriever
+from dotenv import load_dotenv
+load_dotenv()
+
+# Paths
+TEXT_PATH = Path("../data/text/video.txt")
+
+# Read text
+text = TEXT_PATH.read_text(encoding="utf-8")
+
+# Splitter
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=60
+)
+
+chunks = splitter.split_text(text)
+
+'''print(len(chunks))
+for idx, chunk in enumerate(chunks, start=1):
+    print(f"\n--- Chunk {idx} ---\n")
+    print(chunk)'''
+
+embeddings=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_store = FAISS.from_texts(
+    texts=chunks,
+    embedding=embeddings
+)
+
+#print(vector_store.index_to_docstore_id)
+
+retriever=vector_store.as_retriever(search_kwargs={"k": 2})
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+question="was the topiv of llm was discussed in the video? If yes then what was discussed ?"
+relevant_chunks=retriever.invoke(question)
+
+
+context = "\n\n".join(doc.page_content for doc in relevant_chunks)
+
+final_prompt = prompt.format(
+    context=context,
+    question=question
+)
+
+#print(final_prompt)
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant", 
+    temperature=0.2
+)
+
+answer=llm.invoke(final_prompt)
+print(answer)
